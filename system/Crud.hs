31fc8ed8bb96a649447926c49c8f08e5b70890ff
Breno Mariz <mrzbeo@gmail.com>
2017-09-03 05:11:05 -0300

    Remove person logic
    Signed-off-by: Breno Mariz <mrzbeo@gmail.com>
    Signed-off-by: Mateus de Morais <mateusmorais@hotmail.com.br>

--- system/Crud.hs
+++ system/Crud.hs
@@ -52,3 +52,18 @@ createPerson main_tree = do
     getChar >>= putChar
     let new_tree = insertPersonTree person main_tree
     return new_tree
+
+--removePerson :: String -> Arvbin a -> Arvbin a
+removePerson x Null = Null
+remove x (Node y left right)
+    | x == firstName y = deletePerson (Node y left right)
+    | x < firstName y = (Node y (remove x left) right)
+    | otherwise = (Node y left (remove x right))
+
+deletePerson (Node y Null right) = right
+deletePerson (Node y left Null) = left
+deletePerson (Node y left right) = (Node (getLeftmost right) left (remove (firstName(getLeftmost right)) right))
+
+getLeftmost (Node y Null right) = y
+getLeftmost (Node y left right) = getLeftmost left
+        
